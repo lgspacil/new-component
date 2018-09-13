e19c15f87572b938649a289a74c68b69cafa0e21
Lucas G Spacil <lucasspacil1@Lucass-MacBook-Pro.local>
2018-09-13 14:55:04 -0700

    web

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import { isMobile } from 'mobile-device-detect';
 
-class CoolComponent extends Component {
-    render(){
-        return (
-            <View>
-                <Text>I am a cool component!</Text>
-            </View>
-        )
+if (isMobile){
+    
+    import { View, Text } from 'react-native';
+
+    class CoolComponent extends Component {
+        render(){
+            return (
+                <View>
+                    <Text>I am a cool component in mobile!</Text>
+                </View>
+            )
+        }
     }
+    
+    export default CoolComponent;
+
+}else {
+    class CoolComponent extends Component {
+        render(){
+            return (
+                <div>
+                    <p>I am a cool component in the web!</p>
+                </div>
+            )
+        }
+    }
+
+    export default CoolComponent;
 }
 
-export default CoolComponent;
